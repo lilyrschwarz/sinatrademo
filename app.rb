e8ed8ac6eff547238053ec111f2115130212898d
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-16 23:51:15 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/json'
 
 get '/' do
-    @kind_of_cake  = File.open('cake.list', 'r')
+    @list  = File.open('cake.list', 'r')
     @counter = 1
     erb :list
 end
