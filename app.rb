5ee014443e38f31ae25c4514eabcde0b8a39116c
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-16 23:35:44 -0700

    list of cake

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/json'
 
 get '/' do
-  "Hello, world"
+    @kind_of_cake  = File.open('cake.list', 'r')
+    @counter = 1
+    erb :list
+end
+
+get '/json' do
+    json File.read('cake.list')
 end
